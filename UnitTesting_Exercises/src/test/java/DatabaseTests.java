07d707f342e403c7ec8b27f649dc9c16d4c265c9
SophYo <sophiya.dimitrova@gmail.com>
2019-08-01 17:53:16 +0300

    test methods added

--- UnitTesting_Exercises/src/test/java/DatabaseTests.java
+++ UnitTesting_Exercises/src/test/java/DatabaseTests.java
@@ -1,25 +1,38 @@
+import org.junit.Assert;
 import org.junit.Test;
 import p01_Database.Database;
 
 import javax.naming.OperationNotSupportedException;
+import java.lang.reflect.Field;
 
 public class DatabaseTests {
 
     private static final Integer[] INITIAL_ELEMENTS = new Integer[]{1, 2, 3};
 
     @Test(expected = OperationNotSupportedException.class)
-    public void constructorShouldThrowExceptionWithLessThanOneElement() throws OperationNotSupportedException {
+    public void constructorShouldThrowExceptionWithLessThanOneElement()
+            throws OperationNotSupportedException {
         Database database = new Database();
     }
 
     @Test(expected = OperationNotSupportedException.class)
-    public void constructorShouldThrowExceptionWithMoreThanSixteenElement() throws OperationNotSupportedException {
+    public void constructorShouldThrowExceptionWithMoreThanSixteenElement()
+            throws OperationNotSupportedException {
         Database database = new Database();
     }
 
     @Test
-    public void constructingInterfaceShouldIncreaseElementsCount() throws OperationNotSupportedException {
-        Database database = new Database(1, 2, 3);
+    public void constructingDatabaseShouldIncreaseElementsCount()
+            throws OperationNotSupportedException, NoSuchFieldException, IllegalAccessException {
+        Database database = new Database(DatabaseTests.INITIAL_ELEMENTS);
+
+        Field elementsCount = Database.class.getDeclaredField("elementsCount");
+        elementsCount.setAccessible(true);
+
+        //here we provide the obj that we wont to take the field from
+        int fieldCurrentValue = elementsCount.getInt(database);
+
+        Assert.assertEquals(DatabaseTests.INITIAL_ELEMENTS.length, fieldCurrentValue);
     }
 
 }
