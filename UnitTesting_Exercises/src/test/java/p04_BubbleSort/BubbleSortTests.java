2495025fec6b0d51bfdb76b23154759fec320959
SophYo <sophiya.dimitrova@gmail.com>
2019-08-01 22:10:32 +0300

    test method added

--- UnitTesting_Exercises/src/test/java/p04_BubbleSort/BubbleSortTests.java
+++ UnitTesting_Exercises/src/test/java/p04_BubbleSort/BubbleSortTests.java
@@ -1,4 +1,28 @@
 package p04_BubbleSort;
 
+import org.junit.Assert;
+import org.junit.Test;
+import p04_BubbleSortTest.Bubble;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
 public class BubbleSortTests {
+
+    private int[] elements = {13, 42, 69, -1, -2, -34};
+
+    private int[] sorted = Arrays.stream(elements)
+            .sorted()
+            .toArray();
+
+    @Test
+    public void sortShouldSortTheCollection() {
+        Bubble.sort(elements);
+        Assert.assertArrayEquals(sorted,elements);
+
+    }
+
+
 }
