7b387db3cbf5dec8924657865802ca6bba424603
SophYo <sophiya.dimitrova@gmail.com>
2019-08-01 22:40:51 +0300

    first test method finished

--- UnitTesting_Exercises/src/test/java/p06_TirePressureMonitoringSystemTests/AlarmTest.java
+++ UnitTesting_Exercises/src/test/java/p06_TirePressureMonitoringSystemTests/AlarmTest.java
@@ -1,4 +1,22 @@
 package p06_TirePressureMonitoringSystemTests;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import p06_TirePressureMonitoringSystem.Alarm;
+import p06_TirePressureMonitoringSystem.Sensor;
+
 public class AlarmTest {
+
+    @Test
+    public void alarmShouldBeOnIfOutOfScope() {
+        Alarm alarm = new Alarm();
+        Sensor sensor = Mockito.mock(Sensor.class);
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(16d);
+        alarm.setSensor(sensor);
+        alarm.check();
+
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
+
 }
