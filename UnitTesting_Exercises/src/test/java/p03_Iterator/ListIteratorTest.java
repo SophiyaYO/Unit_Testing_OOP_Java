143683baebbf03d7e3aeb26fa397789d49dbb08e
SophYo <sophiya.dimitrova@gmail.com>
2019-08-02 13:24:35 +0300

    created + first Test Method

---
+++ UnitTesting_Exercises/src/test/java/p03_Iterator/ListIteratorTest.java
@@ -0,0 +1,14 @@
+package p03_Iterator;
+
+import org.junit.Test;
+import p03_IteratorTest.ListIterator;
+
+import javax.naming.OperationNotSupportedException;
+
+public class ListIteratorTest {
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void creatingListIteratorShouldThrowExceptionIfNullIsPassedToTheConstructor() throws OperationNotSupportedException {
+       ListIterator listIterator =  new ListIterator(null);
+    }
+}
