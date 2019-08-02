f95580bbe32db6647b86d3894ceb4958a8e1c81d
SophYo <sophiya.dimitrova@gmail.com>
2019-08-02 14:19:44 +0300

    second Test Method added + implementation

--- UnitTesting_Exercises/src/test/java/p05_CustomLinkedListTests/CustomLinkedListTests.java
+++ UnitTesting_Exercises/src/test/java/p05_CustomLinkedListTests/CustomLinkedListTests.java
@@ -14,4 +14,15 @@ public class CustomLinkedListTests {
 
         Assert.assertTrue(list.contains(13));
     }
+
+    @Test
+    public void removingToLinkedListShouldWork() {
+        CustomLinkedList<Integer> list = new CustomLinkedList<>();
+
+        list.add(13);
+        list.remove(13);
+
+        Assert.assertFalse(list.contains(13));
+    }
+
 }
